27f4f85b47f3fad47afa23adb1a0af39490d6694
unknown <heiduk.k.k.s@yandex.ru>
2019-02-09 10:35:16 +0300

    Работает хуже, но задумка классная

--- morphy.py
+++ morphy.py
@@ -4,13 +4,17 @@ morph = pymorphy2.MorphAnalyzer()
 ignorechars = ''',:'"!?;()'''
 ignoreparts = ['PREP', 'CONJ', 'PRCL', 'INTJ']
 subject = ['NOUN', 'NUMR', 'NPRO']
-predicate = ['VERB', 'INFN', 'ADJS']
+predicate = ['VERB', 'INFN', 'ADJS', 'PRTS']
 
-def global_type(word):
-    return morph.parse(word)[0].tag.POS
+def search(word, need_type):
+    variants = morph.parse(word)
+    for i in range(len(variants)):
+        if variants[i].tag.POS in need_type:
+            return i
+    return None
 
-def part_of_speech(word):
-    part = morph.parse(word)[0].tag.POS
+def part_of_speech(word, pos):
+    part = morph.parse(word)[pos].tag.POS
     if part in ignoreparts:
         return 0
     elif part in subject:
@@ -19,14 +23,17 @@ def part_of_speech(word):
         return 2
     return 3
 
-def case(word):
-    x = morph.parse(word)[0].tag.case
-    if morph.parse(word)[0].tag.case == 'nomn':
+def global_type(word, pos):
+    return morph.parse(word)[pos].tag.POS
+
+def case(word, pos):
+    x = morph.parse(word)[pos].tag.case
+    if morph.parse(word)[pos].tag.case == 'nomn':
         return 1
     return 0
 
-def time(word):
-    time_ = morph.parse(word)[0].tag.tense
+def time(word, pos):
+    time_ = morph.parse(word)[pos].tag.tense
     if time_ == 'past':
         return -1
     elif time_ == 'pres':
@@ -35,8 +42,8 @@ def time(word):
         return 1
     return None
 
-def face(word):
-    face_ = morph.parse(word)[0].tag.persons
+def face(word, pos):
+    face_ = morph.parse(word)[pos].tag.persons
     if face_ == '1per':
         return 1
     elif face_ == '2per':
@@ -45,18 +52,18 @@ def face(word):
         return 3
     return None
 
-def kind(word): # род
-    kind_ = morph.parse(word)[0].tag.gender
+def kind(word, pos): # род
+    kind_ = morph.parse(word)[pos].tag.gender
     if kind_ == 'femn':
         return -1
     elif kind_ == 'masc':
         return 1
-    elif kind == 'neut':
+    elif kind_ == 'neut':
         return 0
     return None
 
-def number(word):
-    number_ = morph.parse(word)[0].tag.number
+def number(word, pos):
+    number_ = morph.parse(word)[pos].tag.number
     if number_ == 'sing':
         return -1
     elif number_ == 'plur':
@@ -80,69 +87,94 @@ def clean(text):
 
     now_subj = []
     now_predic = []
+    predicate_flag = False
     for word in text:
-        now_type = part_of_speech(word)
-        if now_type == 1:
-            try:
-                subj[word][0] += 1
-            except:
-                subj[word] = [1, 0]
+        try: # if not subject
+            subj_position = search(word, subject)
+            now_type = part_of_speech(word, subj_position)
+            if now_type == 1:
+                try:
+                    subj[word][0] += 1
+                except:
+                    subj[word] = [1, 0]
 
-            if case(word):
-                subj[word][1] = 1
-                now_subj.append(word)
+                if case(word):
+                    subj[word][1] = 1
+                    now_subj.append(word)
+        except:
+            pass
 
-        elif now_type == 2:
-            try:
-                predic[word] += 1
-            except:
-                predic[word] = 1
-
+        try: # if not predicate
+            predic_position = search(word, predicate)
+            now_type = part_of_speech(word, predic_position)
+            if now_type == 2:
+                try:
+                    predic[word] += 1
+                except:
+                    predic[word] = 1
+                if global_type(word, predic_position) == 'VERB' or global_type(word, predic_position) == 'INFN':
+                    predicate_flag = True
+        except:
+            pass
 
 
     now_predic = list(predic)
-
-    if len(now_subj) == 0:
-        return [now_predic, list(subj)]
-    if len(now_predic) == 0:
-        return now_subj
-
     basis = []
 
-    for i in now_subj:
-        for j in now_predic:
-            if number(i) != number(j) and morph.parse(j)[0].tag.POS != 'INFN':
-                continue
-            elif time(j) == -1 and (kind(i) == kind(j) and global_type(i) != 'NPRO' or global_type(i) == 'NPRO'):
-                basis.append([i, j])
-            elif time(j) != -1 and number(i) == number(j):
-                basis.append([i, j])
+    if len(now_predic) == 0:
+            return now_subj
+    if len(now_subj) == 0:
+        return [now_predic, list(subj)]
 
-    return basis
+    if predicate_flag:
+        for i in now_subj:
+            for j in now_predic:
+                posi = search(i, subject)
+                posj = search(j, predicate)
 
+                if number(i, posi) != number(j, posj) and global_type(j, posj) != 'INFN':
+                    continue
+                #if global_type(i) == 'NPRO':
+                #    basis.append([i, j])
+                elif time(j, posj) == -1 and (kind(i, posi) == kind(j, posj) and global_type(i, posi) != 'NPRO' or global_type(i, posi) == 'NPRO'):
+                    basis.append([i, j])
+                elif time(j, posj) != -1 and number(i, posi) == number(j, posj):
+                    basis.append([i, j])
 
-docs = [
-    "Я тихонечко ворчал в углу",
+        return basis
+
+    else:
+        pass
+
+docs_ = [
+    #"Я тихонечко ворчал в углу",
     "Я делало делал делала уделали ворчал",
     "Лисица прыгнула побежал поскакало улетели делает сделают нарисует",
     "Сходив туда, прыгнул на шкаф",
-	"Британская полиция знает о местонахождении основателя WikiLeaks",
-	"В суде США начинается процесс против россиянина, рассылавшего спам",
+	#"Британская полиция знает о местонахождении основателя WikiLeaks",
+	#"В суде США начинается процесс против россиянина, рассылавшего спам",
 	"Церемонию вручения Нобелевской премии мира бойкотируют 19 стран",
 	"В Тамбове проходят массовые проверки газового оборудования",
-	"Планшет Lenovo Phab 3 получит экран диагональю 7,8 дюйма",
-	"Шведский суд отказался рассматривать апелляцию основателя Wikileaks",
-	"Вышло четвёртое издание «Книги Памяти»",
-	"Полиция Великобритании нашла основателя WikiLeaks, но, не арестовала",
-	"В Стокгольме и Осло сегодня состоится вручение Нобелевских премий",
-    "Названы 10 претендентов на титул лучшего в мире автомобиля",
+	#"Планшет Lenovo Phab 3 получит экран диагональю 7,8 дюйма",
+	#"Шведский суд отказался рассматривать апелляцию основателя Wikileaks",
+	#"Вышло четвёртое издание «Книги Памяти»",
+	#"Полиция Великобритании нашла основателя WikiLeaks, но, не арестовала",
+	#"В Стокгольме и Осло сегодня состоится вручение Нобелевских премий",
+    #"Названы 10 претендентов на титул лучшего в мире автомобиля",
     "Возвращаясь домой, мне стало грустно",
     "Книга была прочитана за считанные часы"
 ]
 
+docs = open("input.txt", "r").read().replace('\n', ' ').split('.')
+del docs[len(docs) - 1]
 
-for i in docs:
-    print(i)
-    text = clean(i)
-    print(text)
-    print()
+
+file_ = open("output.txt", "w")
+
+for i in docs_:
+    file_.write(i)
+    file_.write('\n')
+    file_.write(str(clean(i)))
+    file_.write('\n')
+
+file_.close()
